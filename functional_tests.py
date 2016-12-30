0a21ca5082895a0fa6dbf6eb21a499f83456b5ad
Hanif Fakira <fakira.work@gmail.com>
2016-12-30 16:41:31 +0000

    $1.5 FT new FT add another item

--- functional_tests.py
+++ functional_tests.py
@@ -40,13 +40,24 @@ class NewVisitorTest(unittest.TestCase):
 
         # There is still a text box inviting him to add another item. He
         # enters "Use peacock feathers to make a fly" (Han is very methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
         # The page updates again, and now shows both items on his list
-        self.fail('Finish the test!')
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly' ,
+             [row.text for row in rows]
+        )
 
 
         # Han wonders whether the site will remember his list. Then he sees
         # that the site has generated a unique URL for his -- there is some
         # explanatory text to that effect.
+        self.fail('Finish the test!')
 
         # He visits that URL - his to-do list is still there.
 
