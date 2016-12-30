52755d4c0293624b48a16b4dd548d68b9614a8ec
Hanif Fakira <fakira.work@gmail.com>
2016-12-30 18:05:14 +0000

    $1.5 UT dont save blank items

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -8,6 +8,10 @@ from lists.models import Item
 # Create your tests here.
 class HomePageTest(TestCase):
 
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+        
     def test_uses_home_template(self):
         response = self.client.get('/')
 
@@ -16,7 +20,7 @@ class HomePageTest(TestCase):
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
 
-        self.assertEqual(Item.objects.count(), 1)  
+        self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
---
+++ to-do.md
@@ -0,0 +1,7 @@
+fix post method test, need to extract db stuff
+
+fix view, saving empty data when post
+
+Display multiple items in the table
+
+Support more than one list!
