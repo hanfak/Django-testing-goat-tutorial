08399074584cfcd5f0a80cfd5dae35d8fd74a7d3
Hanif Fakira <fakira.work@gmail.com>
2016-12-30 12:17:02 +0000

    $1.2 failing ft, use unittest, mvp in comments

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,41 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Han has heard about a cool new online to-do app. He goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # He notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # He types "Buy peacock feathers" into a text box (Han's hobby
+        # is tying fly-fishing lures)
+
+        # When he hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+
+        # There is still a text box inviting him to add another item. He
+        # enters "Use peacock feathers to make a fly" (Han is very methodical)
+
+        # The page updates again, and now shows both items on his list
+
+        # Han wonders whether the site will remember his list. Then he sees
+        # that the site has generated a unique URL for his -- there is some
+        # explanatory text to that effect.
+
+        # He visits that URL - his to-do list is still there.
+
+        # Satisfied, he goes back to sleep
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
