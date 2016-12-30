3ff8761a7b5b323e75742a450e4bd1229b6c8b80
Hanif Fakira <fakira.work@gmail.com>
2016-12-30 16:35:18 +0000

    $1.5 UT template used, all UT pass instead of new response

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    if request.method == 'POST':
-        return HttpResponse(request.POST['item_text'])
-    return render(request, 'home.html')
+    # if request.method == 'POST':
+    #     return HttpResponse(request.POST['item_text'])
+    context = {
+        'new_item_text': request.POST.get('item_text', ''),
+    }
+    return render(request, 'home.html', context)
