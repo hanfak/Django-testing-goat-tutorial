e422c1743bbc7ad7e7692974cfacfd76ad2ef681
Hanif Fakira <fakira.work@gmail.com>
2016-12-31 08:38:43 +0000

    $1.6 RT new url and view set

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -12,3 +12,7 @@ def home_page(request):
 
     context = {'items' : Item.objects.all()}
     return render(request, 'home.html', context)
+
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
