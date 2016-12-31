ba922c2e3605677fba02d134f10ca45e33119b46
Hanif Fakira <fakira.work@gmail.com>
2016-12-31 11:01:49 +0000

    $1.7 FT input box in middle in list.html

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -119,3 +119,13 @@ class NewVisitorTest(LiveServerTestCase):
             512,
             delta=5
         )
+
+        # He starts a new list and sees the input is nicely
+        # centered there too
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
